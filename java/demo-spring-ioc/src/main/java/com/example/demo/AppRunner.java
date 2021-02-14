873ef213c226b3991e602ea0079bfbb71e0564e9
phantom08266 <phantom08266@gmail.com>
2021-02-14 20:25:29 +0900

    ResourceLoader처리 https://github.com/phantom08266/TIL/issues/18

--- java/demo-spring-ioc/src/main/java/com/example/demo/AppRunner.java
+++ java/demo-spring-ioc/src/main/java/com/example/demo/AppRunner.java
@@ -5,17 +5,25 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
-    ApplicationEventPublisher publishEvent;
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(publishEvent.getClass());
-        publishEvent.publishEvent(new MyEvent(this, 100));
-
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+        System.out.println(Files.readString(Path.of(resource.getURI())));
     }
 }
