842fdf8e7eafeba49418ff83b0b8c24e4e45c407
phantom08266 <phantom08266@gmail.com>
2021-02-14 17:26:49 +0900

    MessageSource처리 https://github.com/phantom08266/TIL/issues/16

--- java/demo-spring-ioc/src/main/java/com/example/demo/AppRunner.java
+++ java/demo-spring-ioc/src/main/java/com/example/demo/AppRunner.java
@@ -5,29 +5,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 @Component
 public class AppRunner  implements ApplicationRunner {
     @Autowired
-    ApplicationContext applicationContext;
-
-    @Autowired
-    BookRepository bookRepository;
-
-    @Value("${app.name}")
-    String appName;
+    MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Environment environment = applicationContext.getEnvironment();
-        System.out.println(Arrays.toString(environment.getActiveProfiles()));
-        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
-        System.out.println(environment.getProperty("app.name"));
-        System.out.println(environment.getProperty("app.name"));
-        System.out.println(appName);
+        while(true){
+            System.out.println(messageSource.getClass());
+            System.out.println(messageSource.getMessage("greeting", new String[]{"june"}, Locale.US));
+            System.out.println(messageSource.getMessage("greeting",new String[]{"june"}, Locale.getDefault()));
+            Thread.sleep(1000L);
+        }
+
     }
 }
--- java/demo-spring-ioc/src/main/java/com/example/demo/DemoApplication.java
+++ java/demo-spring-ioc/src/main/java/com/example/demo/DemoApplication.java
@@ -3,15 +3,25 @@ package com.example.demo;
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
-@PropertySource("classpath:/app.properties")
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource reloadableResourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+        reloadableResourceBundleMessageSource.setBasename("classpath:/messages");
+        reloadableResourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        reloadableResourceBundleMessageSource.setCacheSeconds(3);
+        return reloadableResourceBundleMessageSource;
+    }
 }
