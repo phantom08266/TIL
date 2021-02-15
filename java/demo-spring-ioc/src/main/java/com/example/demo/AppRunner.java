4ef556125f9e9f0363e23fcb06129d782f9b5f19
phantom08266 <phantom08266@gmail.com>
2021-02-15 19:44:45 +0900

    SpEL 설정 https://github.com/phantom08266/TIL/issues/23

--- java/demo-spring-ioc/src/main/java/com/example/demo/AppRunner.java
+++ java/demo-spring-ioc/src/main/java/com/example/demo/AppRunner.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.convert.ConversionService;
@@ -9,12 +10,40 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    ConversionService conversionService;
+    @Value("#{1 + 1}")
+    int value;
+
+    @Value("#{'hello ' + 'world'}")
+    String greeting;
+
+    @Value("#{1 eq  1}")
+    boolean trueOrFalse;
+
+    @Value("hello")
+    String hello;
+
+    @Value("${my.value}")
+    int myValue;
+
+    @Value("#{${my.value} eq 200}")
+    boolean isMyValue;
+
+    @Value("#{'spring'}")
+    String spring;
+
+    @Value("#{sample.data}")
+    int sampleData;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(conversionService);
-        System.out.println(conversionService.getClass().toString());
+        System.out.println("============");
+        System.out.println(value);
+        System.out.println(greeting);
+        System.out.println(trueOrFalse);
+        System.out.println(hello);
+        System.out.println(myValue);
+        System.out.println(isMyValue);
+        System.out.println(spring);
+        System.out.println(sampleData);
     }
 }
---
+++ java/demo-spring-ioc/src/main/java/com/example/demo/Sample.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+    public int data = 100;
+}
