e985858b2257f1fbfe49efce900283c71dd12cc9
phantom08266 <phantom08266@gmail.com>
2021-02-19 01:42:32 +0900

    핸들러 인터셉트 처리

--- java/demo-spring-boot/src/main/java/com/example/demospringboot/WebConfig.java
+++ java/demo-spring-boot/src/main/java/com/example/demospringboot/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.demospringboot;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 스프링 MVC에서 사용할 경우
@@ -13,6 +14,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //    }
 //}
 @Configuration
-public class WebConfig{
-
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .order(-1)
+                .addPathPatterns("/hi");
+    }
 }
