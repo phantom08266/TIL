7ce52b3312f05b6ff21528933f295f5add79c358
phantom08266 <phantom08266@gmail.com>
2022-06-19 17:29:24 +0900

    test [14장] 프랑->달러 환율 계산을 위한 실패 테스트 케이스 작성

--- 도서/Tdd/src/test/java/tdd/DollarTest.java
+++ 도서/Tdd/src/test/java/tdd/DollarTest.java
@@ -60,4 +60,12 @@ class DollarTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertThat(Money.dollar(1)).isEqualTo(result);
     }
+
+    @Test
+    void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertThat(Money.dollar(1)).isEqualTo(result);
+    }
 }
