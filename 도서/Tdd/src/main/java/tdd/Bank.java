362514aa9c5df888e0b03a0e8b2e2a4f86d87e51
phantom08266 <phantom08266@gmail.com>
2022-06-19 17:42:58 +0900

    feat [14장] 환율에 대한 처리를 Money->Bank로 변경

--- 도서/Tdd/src/main/java/tdd/Bank.java
+++ 도서/Tdd/src/main/java/tdd/Bank.java
@@ -3,7 +3,11 @@ package tdd;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    public int rate(String from, String to){
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 
     public void addRate(String chf, String usd, int i) {
