d404e5d0d16caf2a38c82012ecde1c7b327c1d99
phantom08266 <phantom08266@gmail.com>
2022-06-26 21:46:37 +0900

    Feat [18장] Reflection을 사용하여 메서드 이름으로 호출되도록 수정

--- 도서/Tdd/src/main/java/xunit/WasRun.java
+++ 도서/Tdd/src/main/java/xunit/WasRun.java
@@ -1,11 +1,16 @@
 package xunit;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WasRun {
 
     public int wasRun;
+    private final String name;
 
     public WasRun(String name) {
         this.wasRun = 0;
+        this.name = name;
     }
 
     public void testMethod() {
@@ -13,7 +18,17 @@ public class WasRun {
     }
 
     public void run() {
-        this.testMethod();
+        getattr();
+    }
+
+    private void getattr() {
+        try {
+            Class<WasRun> wasRun = WasRun.class;
+            Method method = wasRun.getMethod(name);
+            method.invoke(this);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
 }
