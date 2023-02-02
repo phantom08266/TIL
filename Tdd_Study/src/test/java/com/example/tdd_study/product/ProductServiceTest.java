03dfcc0363a91341ed2ad6d4252f64388992101a
phantom08266 <phantom08266@gmail.com>
2023-02-03 07:44:00 +0900

    Feat [TDD]: 성공하는 상품 등록 테스트 코드 작성

--- Tdd_Study/src/test/java/com/example/tdd_study/product/ProductServiceTest.java
+++ Tdd_Study/src/test/java/com/example/tdd_study/product/ProductServiceTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductServiceTest {
 
     private ProductService productService;
+    private ProductPort productPort;
+    private ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
-        productService = new ProductService();
+        productRepository = new ProductRepository();
+        productPort = new ProductAdapter(productRepository);
+        productService = new ProductService(productPort);
     }
 
     @Test
@@ -35,8 +42,68 @@ public class ProductServiceTest {
     }
 
     private class ProductService {
+        private final ProductPort productPort;
+
+        private ProductService(ProductPort productPort) {
+            this.productPort = productPort;
+        }
+
         public void addProduct(AddProductRequest request) {
-            throw new UnsupportedOperationException("Unsupported addProduct");
+            Product product = new Product(request.name(), request.price(), request.discountPolicy());
+            productPort.save(product);
+        }
+    }
+
+    private class Product {
+        private Long id;
+        private final String name;
+        private final int price;
+        private final DiscountPolicy discountPolicy;
+
+        public Product(String name, int price, DiscountPolicy discountPolicy) {
+            Assert.hasText(name, "상품명을 입력해주세요.");
+            Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
+            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+            this.name = name;
+            this.price = price;
+            this.discountPolicy = discountPolicy;
+        }
+
+        public void assignId(Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+    }
+
+    private interface ProductPort {
+        void save(Product product);
+    }
+
+    private class ProductAdapter implements ProductPort {
+
+        private final ProductRepository productRepository;
+
+        private ProductAdapter(ProductRepository productRepository) {
+            this.productRepository = productRepository;
+        }
+
+        @Override
+        public void save(final Product product) {
+            productRepository.save(product);
+        }
+    }
+
+    private class ProductRepository {
+
+        private Long sequence = 0L;
+        private Map<Long, Product> persistence = new HashMap<>();
+
+        public void save(Product product) {
+            product.assignId(++sequence);
+            persistence.put(product.getId(), product);
         }
     }
 }
