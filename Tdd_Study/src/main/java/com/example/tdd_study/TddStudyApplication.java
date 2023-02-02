792d24672ae7cbf3cdfbf79433af0742fc313d0e
phantom08266 <phantom08266@gmail.com>
2023-02-03 07:16:51 +0900

    Feat [TDD]: 프로젝트 생성

---
+++ Tdd_Study/src/main/java/com/example/tdd_study/TddStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdd_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddStudyApplication.class, args);
+    }
+
+}
