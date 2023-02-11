23d7b3fef1f2830d3023ebad1d3f61771f3d3a53
phantom08266 <phantom08266@gmail.com>
2023-02-11 20:50:36 +0900

    Feat [TDD]: RestAsserd를 사용하여 실패하는 API요청 테스트 코드 작성

---
+++ Tdd_Study/src/test/java/com/example/tdd_study/product/ProductApiTest.java
@@ -0,0 +1,44 @@
+package com.example.tdd_study.product;
+
+import com.example.tdd_study.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductApiTest extends ApiTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void 상품등록() {
+        final AddProductRequest request = 상품등록_생성요청();
+
+        // API요청
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private AddProductRequest 상품등록_생성요청() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
+        return request;
+    }
+
+}
