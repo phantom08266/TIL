10499d26d3ec22674c27bd7f25b8f7454ed1c046
phantom08266 <phantom08266@gmail.com>
2023-02-11 20:56:58 +0900

    Feat [TDD]: RestAsserd를 사용하여 테스트코드 성공하도록 수정

---
+++ Tdd_Study/src/main/java/com/example/tdd_study/product/ProductController.java
@@ -0,0 +1,19 @@
+package com.example.tdd_study.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addProduct(@RequestBody AddProductRequest addProductRequest) {
+        productService.addProduct(addProductRequest);
+    }
+}
--- Tdd_Study/src/test/java/com/example/tdd_study/product/ProductApiTest.java
+++ Tdd_Study/src/test/java/com/example/tdd_study/product/ProductApiTest.java
@@ -7,7 +7,6 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,15 +21,19 @@ public class ProductApiTest extends ApiTest {
         final AddProductRequest request = 상품등록_생성요청();
 
         // API요청
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+        ExtractableResponse<Response> response = 상품등록_요청(request);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private ExtractableResponse<Response> 상품등록_요청(AddProductRequest request) {
+        return RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(request)
                 .when()
                 .post("/products")
                 .then()
                 .log().all().extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     private AddProductRequest 상품등록_생성요청() {
@@ -40,5 +43,6 @@ public class ProductApiTest extends ApiTest {
         final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
         return request;
     }
+
 
 }
